fdeade1a6c2f98821caac22f71b68327811f7351
Daniil Shapin <daniil.shapin327@gmail.com>
2024-10-25 03:33:52 +0600

    ultraone

--- services.py
+++ services.py
@@ -8,16 +8,28 @@ from models import Users
 
 async def save_user_objects(username: str, objects: dict):
     session: AsyncSession = async_session()
-    await session.execute(sa.insert(Users).values([{
-        "username": username,
-        "objects": objects
-    }]))
+    user = await session.scalar(sa.select(Users).where(Users.username == username))
+    if not user:
+        await session.execute(sa.insert(Users).values([{
+            "username": username,
+            "objects": objects
+        }]))
+    else:
+        await session.execute(sa.update(Users).where(Users.username == username).values({
+
+            "objects": objects
+        }))
+
     await session.commit()
     await session.close()
+
 
 async def get_user_objects(username: str):
     session: AsyncSession = async_session()
     user = await session.scalar(sa.select(Users).where(Users.username == username))
     await session.commit()
     await session.close()
-    return user.objects
+    if user is None:
+        return {}
+    else:
+        return user.objects
